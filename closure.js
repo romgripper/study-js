6aa93100cec89f17893b9b559632aff0a397000b
romgripper <romgripper@gmail.com>
2018-01-31 00:01:37 +0800

    Try closure and namespace

---
+++ closure.js
@@ -0,0 +1,16 @@
+console.log("closure.js");
+
+function makeMultiplier(multiplier) {
+
+	function b() {
+		console.log("multiplier is: " + multiplier);
+	}
+	b();
+
+	return function (x) {
+		return multiplier * x;
+	};
+}
+
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(10));
---
+++ namespace1.js
@@ -0,0 +1,5 @@
+var ns1 = {};
+ns1.name = "Bingo";
+ns1.sayHello = function() {
+	console.log("Hello " + ns1.name);
+};
